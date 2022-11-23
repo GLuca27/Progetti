1791914b8814767519c6fa16364f3690bbdf5082
GLuca27 <gianluca.sciglitano1@gmail.com>
2022-11-23 17:28:39 +0100

    first commit

---
+++ vite-project/src/components/PageTitle.vue
@@ -0,0 +1,19 @@
+<script>
+export default {
+  data() {
+    return {
+      title: 'la mia prima app con vite'
+    }
+  }
+}
+</script>
+
+<template>
+   <h1>{{ title }}</h1>
+</template>
+
+<style scoped>
+h1{
+  color: #fff;
+}
+</style>
